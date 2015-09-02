a2096c0021147e49296e7fbc0ed4075f746c3ee1
Anson Mansfield <anson.mansfield@gmail.com>
2015-09-02 12:23:52 -0600

    Hello World!

---
+++ Hello World/src/Hello World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Anson Mansfield
+// Version     :
+// Copyright   : ©2015 Anson Mansfield. Permission granted to use under the terms of the GNU General Public License version 3.0.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
